1d84230293587762edba6049409d749dbcf044af
tkaessmann <tobias.kaessmann@s24.com>
2014-08-14 17:26:35 +0200

    implements sugg. for empty datasource handling

--- src/main/java/com/s24/search/solr/analysis/jdbc/JndiJdbcReader.java
+++ src/main/java/com/s24/search/solr/analysis/jdbc/JndiJdbcReader.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Joiner;
+import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 
@@ -29,22 +30,22 @@ public class JndiJdbcReader implements JdbcReader {
    /**
     * Logger.
     */
-   private final Logger logger = LoggerFactory.getLogger(getClass());
+   private static final Logger LOGGER = LoggerFactory.getLogger(JndiJdbcReader.class);
 
    /**
     * JNDI name.
     */
    private final String jndiName;
-   
+
    /**
     * SQL.
     */
    private final String sql;
-   
+
    /**
     * The data source.
     */
-   private DataSource dataSource;
+   private Optional<DataSource> dataSource;
 
    /**
     * Constructor.
@@ -76,11 +77,13 @@ public class JndiJdbcReader implements JdbcReader {
    protected void initDatabase() {
       try {
          Context ctx = new InitialContext();
-         logger.info("Looking up {} in JNDI ...", jndiName);
-         this.dataSource = (DataSource) ctx.lookup(jndiName);
+         LOGGER.info("Looking up {} in JNDI ...", jndiName);
+         this.dataSource = Optional.of((DataSource) ctx.lookup(jndiName));
          ctx.close();
       } catch (Exception e) {
-         throw new IllegalArgumentException(e);
+         this.dataSource = Optional.absent();
+         LOGGER.error("The Datasource could not be retrieved because of ", e);
+         // throw new IllegalArgumentException(e);
       }
    }
 
@@ -89,11 +92,17 @@ public class JndiJdbcReader implements JdbcReader {
     */
    @Override
    public Reader getReader() {
-      QueryRunner runner = new QueryRunner(dataSource);
+
+      if (!dataSource.isPresent()) {
+         LOGGER.error("There is no correct datasource given. We will return no synonyms...");
+         return new StringReader("");
+      }
+
+      QueryRunner runner = new QueryRunner(dataSource.get());
       try {
 
          // read lines off jdbc
-         logger.info("Querying for synonyms using {} ...", sql);
+         LOGGER.info("Querying for synonyms using {} ...", sql);
          List<String> content = runner.query(sql, new ResultSetHandler<List<String>>() {
             @Override
             public List<String> handle(ResultSet rs) throws SQLException {
