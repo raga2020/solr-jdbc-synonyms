398aa75789923b0b24aad25cd8cbcb04810f6aca
Markus Heiden <markus.heiden@s24.com>
2015-07-27 15:19:05 +0200

    Fix warnings

--- src/test/java/com/s24/search/solr/analysis/jdbc/JdbcStopFilterFactoryTest.java
+++ src/test/java/com/s24/search/solr/analysis/jdbc/JdbcStopFilterFactoryTest.java
@@ -60,8 +60,8 @@ public class JdbcStopFilterFactoryTest extends LuceneTestCase {
    public void create() throws Exception {
       Map<String, String> args = new HashMap<>();
       args.put(AbstractAnalysisFactory.LUCENE_MATCH_VERSION_PARAM, Version.LUCENE_5_0_0.toString());
-      args.put(JdbcReaderFactoryParams.DATASOURCE.toString(), "java:comp/env/dataSource");
-      args.put(JdbcReaderFactoryParams.SQL.toString(), "select stopword from stopwords");
+      args.put(JdbcReaderFactoryParams.DATASOURCE, "java:comp/env/dataSource");
+      args.put(JdbcReaderFactoryParams.SQL, "select stopword from stopwords");
 
       // White space tokenizer, to lower case tokenizer.
       MockTokenizer tokenizer = new MockTokenizer();
